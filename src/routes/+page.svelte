98450cabbf08e8ae13bb3bfdbc5e758fc368bb87
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2024-11-05 18:29:10 +0000

    mobile homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <!-- <h1>UniversityDB development in progress.</h1> -->
 <div class='sm:pt-8'>
 
-    <h1 class='text-6xl font-semibold pb-2'>Loughborough University</h1>
-    <h2 class='text-4xl text-gray-400 pb-4'>Loughborough, Leicestershire, United Kingdom</h2>
-    <div class="grid w-full grid-cols-6 gap-6">
-        <div class="card col-span-4 row-span-2">
+    <h1 class='text-2xl sm:text-6xl font-semibold sm:pb-2'>Loughborough University</h1>
+    <h2 class='text-lg sm:text-4xl text-gray-400 sm:pb-4'>Loughborough, Leicestershire, United Kingdom</h2>
+    <div class="grid w-full grid-cols-6 gap-2 sm:gap-6">
+        <div class="card col-span-6 sm:col-span-4 row-span-2">
             <h2>Rankings</h2>
             <table>
                 <thead>
                     <tr>
                         <th>Ranking Organisation</th>
                         <th>Position</th>
-                        <th>Change YoY</th>
+                        <th class='hidden sm:inline'>Change YoY</th>
                         <th>Notes</th>
                     </tr>
                 </thead>
@@ -19,13 +19,13 @@
                     <tr>
                         <td>Quacquarelli Symonds (QS)</td>
                         <td>224</td>
-                        <td>-12</td>
+                        <td class='hidden sm:inline'>-12</td>
                         <td>Worldwide Ranking</td>
                     </tr>
                     <tr>
                         <td>Academic Ranking of World Universities (ARWU), <br>(Shanghai Ranking)</td>
                     <td>701-800</td>
-                    <td>
+                    <td class='hidden sm:inline'>
 
                     </td>
                     <td>Worldwide Ranking</td>
@@ -33,29 +33,29 @@
                     <tr>
                         <td>Times Higher Education (THE)</td>
                         <td>301-350</td>
-                        <td></td>
+                        <td class='hidden sm:inline'></td>
                         <td>Worldwide Ranking</td>
                     </tr>
                     <tr>
                         <td>US News</td>
                         <td>527</td>
-                        <td></td>
+                        <td class='hidden sm:inline'></td>
                         <td>Worldwide Ranking</td>
                     </tr>
                     <tr>
                         <td>Guardian</td>
                         <td>10</td>
-                        <td></td>
+                        <td class='hidden sm:inline'></td>
                         <td>National Ranking (UK)</td>
                     </tr>
                 </tbody>
             </table>
         </div>
-        <div class="card col-span-2">
+        <div class="card col-span-3 sm:col-span-2">
             <h2>Total Students</h2>
             <h3>19,451</h3>
         </div>
-        <div class="card col-span-2">
+        <div class="card col-span-3 sm:col-span-2">
             <h2>Total Faculty</h2>
             <h3>1,305</h3>
         </div>
